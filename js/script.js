429812d347eef0671640c3df0c4cafdba50c77bc
nam <nampham2120@gmail.com>
2020-03-09 12:38:14 +0700

    add feature: sort items

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
 function createDivElementWidthTagName(tagName) {
-    
+
     let div = document.createElement('div');
     div.className = tagName
     return div
 }
 
 function addTodoItem(data) {
-    
+
     let todo = createDivElementWidthTagName("todo-item")
     let card = createDivElementWidthTagName("card")
     let cardTitle = createDivElementWidthTagName("title")
 
-    cardTitle.innerHTML = `<h3>${data.title}</h3>`
+    cardTitle.innerHTML = `<h3>${data.title}</h3>  <button type="button" onClick="addNewItem(${data.id})">+</button> `
 
     card.appendChild(cardTitle)
 
@@ -21,9 +21,9 @@ function addTodoItem(data) {
         let subTitle = createDivElementWidthTagName("title")
         let desc = createDivElementWidthTagName("desc")
 
-        subTitle.innerHTML = `<h3>${json.subTitle}</h3>`
+        subTitle.innerHTML = `<h3>${json.order + ". " + json.subTitle}</h3>`
         desc.innerHTML = `<h4>description:</h4><p>${json.desc}</p>`
-        
+
         subItem.appendChild(subTitle)
         subItem.appendChild(desc)
 
@@ -34,7 +34,25 @@ function addTodoItem(data) {
     document.getElementsByClassName('content')[0].appendChild(todo);
 }
 
+function sortTodoItem(item_list) {
 
+    return item_list.sort(function (a, b) {
+
+        return a.order - b.order
+    })
+}
+
+async function addNewItem(listId) {
+    
+    let newData = await fetch(`http://5e5e2557725f320014ed10b3.mockapi.io/lists/${listId}/tasks`, { method: "POST" })
+    let json = await newData.json()
+    console.log("add new item (",listId,"): ",json.title)
+    
+    /* reload browser when update data */
+    // location.reload(); 
+
+    return json
+}
 
 (async function getData() {
 
@@ -44,19 +62,23 @@ function addTodoItem(data) {
 
     /* get task */
     jsonData.forEach(async item => {
-        console.log(item.name)
+
         let task = await fetch(`http://5e5e2557725f320014ed10b3.mockapi.io/lists/${item.id}/tasks`)
 
-        let jsonTask = await task.json()
+        let itemObj = await task.json()
 
-        console.log(jsonTask)
+        /* sort item by order value */
+        itemObj = sortTodoItem(itemObj)
 
-        let itemArr = jsonTask.map((e) => ({subTitle: e.title, "desc": e.desc}))
+        let itemArr = itemObj.map((e) => ({ subTitle: e.title, "desc": e.desc, "order": e.order}))
 
+        /* add item into html code */
         addTodoItem({
-            title: item.name, 
+            title: item.name,
+            "id": item.id,
             item: itemArr
         })
         //console.log(jsonTask)
     })
 })()
+
